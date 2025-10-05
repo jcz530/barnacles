3f489d6dbc96c6d621198061d8c16c69c171a923
Joe Czubiak <joeczubiak@gmail.com>
2025-10-05 15:34:12 -0700

    add db changes for archived and favorites

---
+++ migrations/0008_worried_star_brand.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `projects` ADD `is_favorite` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `projects` ADD `archived_at` integer;
--- migrations/0009_add_archived_at.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `projects` ADD `is_favorite` integer DEFAULT 0 NOT NULL;
-ALTER TABLE `projects` ADD `archived_at` integer;
